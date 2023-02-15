8a3a5ee1f4aeabff65a3c1024e43cb40a38b9b40
Sameer <samirsaini99@gmail.com>
2023-02-15 12:46:53 +0530

    Appointments screen setup

--- screens/Appointments/index.tsx
+++ screens/Appointments/index.tsx
@@ -1,11 +1,13 @@
+import styles from '@styles/pages/Appointments';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Appointments = () => {
     return (
-        <View>
+        <SafeAreaView style={styles.container}>
             <Text>Appointments</Text>
-        </View>
+        </SafeAreaView>
     );
 };
 
---
+++ styles/pages/Appointments.ts
@@ -0,0 +1,12 @@
+import { COLORS } from '@constants/Colors';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.white,
+        padding: 16,
+    },
+});
+
+export default styles;
